dc1b3bc93880a7aee8e8c4840f4257ea697612c0
Yuri Braga <yurirenanbs@gmail.com>
2024-07-10 20:46:20 -0300

    rust-analyzer working

---
+++ rust/src/main.rs
@@ -0,0 +1,9 @@
+// compile with rustc <file_name.rs>
+// execute with ./file_name
+
+// Creating a new directory: $ cargo new <dir_name>
+// Code in ./src
+fn main() {
+    println!("Hello, World!");
+    println!("I'm a Rustacean!");
+}
